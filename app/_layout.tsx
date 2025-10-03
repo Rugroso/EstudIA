696bfc5ca4263a6242cd50805d4b28913abe1f54
Rugroso <133253941+Rugroso@users.noreply.github.com>
2025-10-02 20:30:02 -0700

    UI issues in mobile got solved

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,19 +2,19 @@ import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DarkTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="classroom" />
-      </Stack>
+    <ThemeProvider value={DarkTheme}>
+      <SafeAreaProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="classroom" />
+        </Stack>
+      </SafeAreaProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
